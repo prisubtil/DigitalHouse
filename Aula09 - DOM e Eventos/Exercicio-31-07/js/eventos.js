7a65495782968cf2863691d80ecf9dfe4d708afd
Priscila Subtil <prisubtil@hotmail.com>
2020-08-01 12:33:36 -0300

    add ex de eventosJS

---
+++ Aula09 - DOM e Eventos/Exercicio-31-07/js/eventos.js
@@ -0,0 +1,23 @@
+function cumprimentar(){
+    alert('Olá');
+}
+
+function alterarFundo(){
+    document.body.style.background = 'green';
+    
+}
+
+function vendoImagem(){
+    console.log('Estou vendo a imagem');
+}
+function clicandoNaImagem(){
+noite.addEventListener("clicandoNaImagem", function(){
+	console.log("Estou clicando na imagem");
+});
+}
+
+function mudarFundo(){
+    body.addEventListener("mudarFundo", function(){
+        document.body.style.background = 'red';
+    });
+    }
