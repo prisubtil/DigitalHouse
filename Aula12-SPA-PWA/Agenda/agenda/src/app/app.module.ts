ff5ad7695421e7b0213a2b0273a16b90b249f67b
Priscila Subtil <prisubtil@hotmail.com>
2020-08-07 22:08:42 -0300

    a

--- Aula12-SPA-PWA/Agenda/agenda/src/app/app.module.ts
+++ Aula12-SPA-PWA/Agenda/agenda/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PainelAgendaComponent } from './painel-agenda/painel-agenda.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PainelAgendaComponent
   ],
   imports: [
     BrowserModule,
