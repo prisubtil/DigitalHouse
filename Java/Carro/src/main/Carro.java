0fe4525a7f390c6d6cff208aaa7d28005ac518ec
Priscila Subtil <prisubtil@hotmail.com>
2020-08-15 13:55:52 -0300

    add ex carro java

---
+++ Java/Carro/src/main/Carro.java
@@ -0,0 +1,19 @@
+package main;
+
+public class Carro {
+	
+		public String fabricante;
+		public String modelo;
+		public String cor;
+		public String tipoCombustivel;
+		public int anoDeFabricacao;
+		public double valorDeMercado;
+		public Proprietario dono;
+		
+		public void ligar() {
+			System.out.println("Ligando carro " + modelo);
+			}
+
+		}
+
+
