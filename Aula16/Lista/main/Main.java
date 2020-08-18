8736b82ac3ac93fd01aea5bc6b5923cfc3cb3a7a
Priscila Subtil <prisubtil@hotmail.com>
2020-08-17 22:41:19 -0300

    lista-java

--- Aula16/Lista/main/Main.java
+++ Aula16/Lista/main/Main.java
@@ -1,25 +1,70 @@
 package main;
 
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		
-		List<Integer> lista = new ArrayList<>();
+		Locale.setDefault(Locale.US);
 		
-		lista.add(1);
-		lista.add(2);
-		lista.add(3);
-		lista.add(4);
-		lista.add(5);
+		Scanner sc = new Scanner(System.in);
 		
-		for(int i=0; i < lista.size(); i++) {
-			System.out.println(lista.get(i));
+		List<Employee> list = new ArrayList<>();
+		
+		
+		System.out.println("How many employees will be registered? ");
+		int N = sc.nextInt();
+		
+		for(int i=0; i < N ; i++) {
+			System.out.println("Employee #"+ (i+1)+ ":");
+			System.out.print("Id: ");
+			Integer id = sc.nextInt();
+			System.out.print("Name: ");
+			sc.nextLine();
+			String name = sc.nextLine();
+			System.out.print("Salary: ");
+			double salary = sc.nextDouble();
+			
+			Employee emp = new Employee(id, name, salary);
+			
+			list.add(emp);
 		}
 		
+		System.out.println();
+		System.out.println("Enter the employee id that will have salary increase: ");
+		int idsalary = sc.nextInt();
 		
+		Integer pos = position(list, idsalary);
+		if(pos == null) {
+			System.out.println("This id does not exist!");
+		}else {
+			System.out.println("Enter the percentage: ");
+			double percent = sc.nextDouble();
+			list.get(pos).increaseSalary(percent);
+		}
+		
+		System.out.println( );
+		System.out.println("List of Employees: ");
+		for(Employee emp : list) {
+			System.out.println(emp);
+		}
+		
+		sc.close();
+		}
+		
+		public static Integer position(List<Employee> list, int id) {
+			for (int i = 0; i < list.size(); i++) {
+				if(list.get(i).getId() == id) {
+					return i;
+				}
+			}
+			return null;
+		}
 		
 	}
 
-}
+
