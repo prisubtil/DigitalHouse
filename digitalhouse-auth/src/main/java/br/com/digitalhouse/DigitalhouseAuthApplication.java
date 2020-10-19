bbe227087aa30cb80eb441a5a38d0a9a6a631457
Priscila Subtil <prisubtil@hotmail.com>
2020-10-19 20:51:09 -0300

    digitalhouse-auth

---
+++ digitalhouse-auth/src/main/java/br/com/digitalhouse/DigitalhouseAuthApplication.java
@@ -0,0 +1,13 @@
+package br.com.digitalhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalhouseAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalhouseAuthApplication.class, args);
+	}
+
+}
